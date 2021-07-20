58ee4b16db8a4afc4281d6988511047111a98677
diegolins10 <diegofernandeslins@gmail.com>
2021-07-19 22:39:02 -0300

    create exceptions

---
+++ Reserva/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reserva.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
