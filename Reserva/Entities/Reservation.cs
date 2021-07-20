84650e09cae631ec750f3772c73dace0d658f44b
diegolins10 <diegofernandeslins@gmail.com>
2021-07-19 22:39:12 -0300

    class reservation

---
+++ Reserva/Entities/Reservation.cs
@@ -0,0 +1,67 @@
+﻿using Reserva.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reserva.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public double Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn); // retorna a diferenca entre duas datas
+            return (int)duration.TotalDays; //converte para dias
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            else if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
